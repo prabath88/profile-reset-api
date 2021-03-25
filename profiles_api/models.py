e1c355050d0834a256320125e265071aa7eedd39
Prabath Dolawatta <prabath.dolawatta@gallagher.com>
2021-03-25 13:57:21 +1300

    create usermodel

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -11,15 +11,14 @@ class UserProfileManager(BaseUserManager):
         """create new user profile"""
         if not email:
             raise ValueError('user must have an email address')
-
+        name = name
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
 
         user.set_password(password)
         user.save(using=self._db)
 
-        return users
-
+        return user
 
     def create_superuser(self, email, name, password):
         """"create super user"""
@@ -31,21 +30,17 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-
 class UserProfile(AbstractBaseUser,PermissionsMixin):
     """Database Model for users in the system"""
-
     email=models.EmailField(max_length=255, unique = True)
     name =  models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['NAME']
-
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         """retrieve full name of user"""
